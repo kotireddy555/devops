9740f1dd4da59bd2843a9a6effb36b9b510225ae
kotireddy555 <kotireddy555@gmail.com>
2020-03-22 14:16:41 +0000

    @nodeconfigupdate

--- bootstrap-kube.sh
+++ bootstrap-kube.sh
@@ -88,7 +88,7 @@ then
 
   # Join worker nodes to the Kubernetes cluster
   echo "[TASK 9] Join node to Kubernetes Cluster"
-  sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.lxd:/joincluster.sh /joincluster.sh 2>/tmp/joincluster.log
+  sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no k88888888master.lxd:/joincluster.sh /joincluster.sh 2>/tmp/joincluster.log
   bash /joincluster.sh >> /tmp/joincluster.log 2>&1
 
 fi
